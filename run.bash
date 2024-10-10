76af42e2ea3d3df22e1bc3f6d226f37ee24968b5
gumpsky <70840522+gumpsky@users.noreply.github.com>
2024-10-10 20:34:44 +0800

    Delete run.bash

--- run.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=U2_net
-#SBATCH --partition=A100
-#SBATCH --nodes=1
-#SBATCH --output=re_test.out.%j
-#SBATCH --error=re_test.err.%j
-#SBATCH --mem=80G
-#SBATCH --gres=gpu:1
-python3 train.py
